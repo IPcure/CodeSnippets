331ece7134811bd25de8d2875a482d962736391b
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2014-02-19 14:33:28 +0100

    Adds code snippet about raw ICMP sockets in Linux.

---
+++ ICMPRawSocket/icmp_client.c
@@ -0,0 +1,89 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <linux/ip.h>
+#include <linux/icmp.h>
+#include <stdlib.h>
+
+#include <string.h> /* memcpy, strlen */
+#include <stdio.h>
+#include <errno.h>
+
+static unsigned short checksum ( const void * header, int length );
+
+int main ( )
+{
+    // We create the socket
+    int sock = socket ( AF_INET, SOCK_RAW, IPPROTO_ICMP );
+    if ( sock < 0 )
+    {
+        printf ( "Error when creating socket.\n" );
+        return -1;
+    }
+
+    // We set up desination info
+    struct sockaddr_in destination;
+    destination.sin_family = AF_INET;
+    destination.sin_addr.s_addr = inet_addr ( "127.0.0.1" );
+
+    const char * msg = "Ceci est un message top secret.";
+    size_t payload_size = strlen ( msg );
+    size_t packet_length = sizeof ( struct icmphdr ) + payload_size;
+    void * packet = malloc ( packet_length );
+
+    struct icmphdr * icmp = packet;
+    char * payload = ( packet + sizeof ( struct icmphdr ) );
+
+    // We setup ICMP header
+    icmp -> type = ICMP_ECHO;
+    icmp -> code = 0;
+    icmp -> un.echo.id = htons ( getpid ( ) );
+
+    // We setup message
+    // memset ( payload, 'd', payload_size );
+    memcpy ( payload, msg, strlen ( msg ) );
+
+	unsigned short i;
+	for ( i = 0; i <= 1000 ; ++i )
+	{
+		// We set the sequence number
+		icmp -> un.echo.sequence = htons ( i );
+
+		// We compute ICMP checksum
+		// icmp -> checksum = 0;
+		icmp -> checksum = checksum ( packet, packet_length );
+
+		// We send the ICMP packet
+		int ret = sendto ( sock, packet, packet_length, 0, ( struct sockaddr * ) &destination, sizeof destination );
+
+		printf ( "Nous avons envoyé %d octets.\n", ret );
+		sleep ( 1 );
+	}
+
+    free ( packet );
+
+    // We close the socket
+    close ( sock );
+    return 0;
+}
+
+static unsigned short checksum ( const void * header, int length )
+{
+    int sum = 0;
+    const unsigned short * current = header;
+    int remaining = length;
+
+    while ( remaining > 1 )
+    {
+        sum += * current++;
+        remaining -= 2;
+    }
+
+    if ( remaining == 1 )
+    {
+        sum += * current;
+    }
+
+    sum = ( sum >> 16 ) + ( sum & 0xffff );
+    sum += ( sum >> 16 );
+    return ( unsigned short ) ~sum;
+}
---
+++ ICMPRawSocket/icmp_server.c
@@ -0,0 +1,158 @@
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <linux/ip.h>
+#include <linux/icmp.h>
+#include <arpa/inet.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <errno.h>
+
+#include <unistd.h>
+#include <fcntl.h>
+
+static unsigned short checksum ( const void * header, int length );
+static int disable_kernel_auto_icmp_reply ( );
+
+int main ( )
+{
+    if ( getuid ( ) != 0 )
+    {
+        printf ( "Please launch me as root.\n" );
+        return -1;
+    }
+
+    if ( disable_kernel_auto_icmp_reply ( ) )
+    {
+        printf ( "Kernel auto ICMP replies sucessfully disabled.\n" );
+    }
+    else
+    {
+        printf ( "Unable to disable kernel auto ICMP replies.\n" );
+        return -1;
+    }
+
+    // We create the socket
+    int sock = socket ( AF_INET, SOCK_RAW, IPPROTO_ICMP );
+    if ( sock < 0 )
+    {
+        printf ( "Error when creating socket.\n" );
+        return -1;
+    }
+
+    // We set up source info (who is talking to us?)
+    struct sockaddr_in source;
+    socklen_t source_length = sizeof ( source );
+
+    // We prepare memory for receiving packets
+    size_t payload_size = 65500;
+    size_t packet_size = sizeof ( struct iphdr ) + sizeof ( struct icmphdr ) + payload_size;
+    void * packet = malloc ( packet_size );
+    struct iphdr * packet_iphdr = packet;
+    struct icmphdr * packet_icmphdr = ( packet + sizeof ( struct iphdr ) );
+    char * packet_payload = ( packet + sizeof ( struct iphdr ) + sizeof ( struct icmphdr ) );
+
+	printf ( "Waiting for someone to give me an ICMP packet. :)\n" );
+
+    // We receive!
+    for ( ; ; )
+    {
+        int ret = recvfrom ( sock, packet, packet_size, 0, ( struct sockaddr * ) &source, &source_length );
+
+		// Convert source IP to understandable char *.
+        char * source_ip = inet_ntoa ( source.sin_addr );
+
+        switch ( packet_icmphdr -> type )
+        {
+            case ICMP_ECHO:
+                printf ( "Echo request" );
+                break;
+
+            case ICMP_ECHOREPLY:
+                printf ( "Echo reply" );
+                break;
+
+            default:
+                printf ( "Unknown" );
+        }
+
+		// We display ICMP header info, as well as who is talking to us
+        int payload_size = ret - sizeof ( struct iphdr ) - sizeof ( struct icmphdr );
+        printf ( " from %s (%d/%d octets)", source_ip, payload_size, ret );
+        printf ( " ID = %#06x, SEQ = %d/%d\n",
+				ntohs ( packet_icmphdr -> un.echo.id ),
+				ntohs ( packet_icmphdr -> un.echo.sequence ),
+				packet_icmphdr -> un.echo.sequence );
+
+		// We display a byte index
+        int i;
+        for ( i = 1 ; i <= payload_size ; ++i )
+        {
+            printf ( "%2d ", i );
+        }
+        printf ( "\n" );
+
+		// We display the bytes values in hexa
+        unsigned char * current = packet_payload;
+        for ( i = 0 ; i < payload_size ; ++i, ++current )
+        {
+            printf ( "%02x ", * current );
+        }
+        printf ( "\n" );
+
+		// We display the bytes values in ASCII
+        current = packet_payload;
+        for ( i = 0 ; i < payload_size ; ++i )
+        {
+            printf ( "%2c ", * current++ );
+        }
+        printf ( "\n\n" );
+    }
+
+    free ( packet );
+
+    // We close the socket
+    close ( sock );
+    return 0;
+}
+
+static unsigned short checksum ( const void * header, int length )
+{
+    int sum = 0;
+    const unsigned short * current = header;
+    int remaining = length;
+
+    while ( remaining > 1 )
+    {
+        sum += * current++;
+        remaining -= 2;
+    }
+
+    if ( remaining == 1 )
+    {
+        sum += * current;
+    }
+
+    sum = ( sum >> 16 ) + ( sum & 0xffff );
+    sum += ( sum >> 16 );
+    return ( unsigned short ) ~sum;
+}
+
+static int disable_kernel_auto_icmp_reply ( )
+{
+    int fd = open ( "/proc/sys/net/ipv4/icmp_echo_ignore_all", O_WRONLY );
+    if ( fd == -1 )
+    {
+        return 0;
+    }
+
+    int res = write ( fd, "1", 1 );
+    close ( fd );
+
+    if ( res == -1 )
+    {
+        return 0;
+    }
+
+    return 1;
+}
